a18dc8f81c742bb47f0c2f1c8cf7b9c0bd03841e
Amulya Paritosh <amulya1171@gmail.com>
2023-09-13 11:39:25 +0530

    added license

--- login.py
+++ login.py
@@ -1,3 +1,19 @@
+
+# SPDX-License-Identifier: MIT
+# --------------------------------------------------------------
+#
+#             Copyright 2023 - 2023, Amulya Paritosh
+#
+#     This File is part of Auto-Internet-Authentication
+# It is not in any way or form part of Chandigarh University
+#  itself and purly developed to just make the authentication
+#  process easier. This does not involve a bypass of any type
+# nor it is encouraged to do so. It uses basic webscrating and
+#          scripted calls to achieve authentication.
+#
+# --------------------------------------------------------------
+
+
 import argparse
 from getpass import getpass
 
